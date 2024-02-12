918ef73734784f3ec6a32d5df3e8eb0b39074be2
Abdullah <boltholt17@gmail.com>
2024-02-13 00:17:35 +0700

    fix: typo at filter_method

--- dart_collection/lib/filter_method.dart
+++ dart_collection/lib/filter_method.dart
@@ -10,7 +10,7 @@ void main(){
   print(list.take(4)); // take first four numbers -> (1, 2, 3, 4) : Iterable<E>
 
   print(list.skipWhile((value) => value < 6)); // skip element that satisfies the predicate -> (6, 7, 8, 9, 10) : Iterable<E>
-  print(list.takeWhile((value) => value < 6 )); // take elements that satisfy the predicate consecutively until it's false -> (1, 2)
+  print(list.takeWhile((value) => value < 6 )); // take elements that satisfy the predicate consecutively until it's false -> (1, 2, 3, 4, 5)
 
   print(list.where((element) => element % 2 == 0)); // will take every element that satisfies the predicate (test function) -> Iterable<E>
 }
